339a324d64a527daed740787a0f9c5c5c8986a13
chck-m8 <evancottrell89@gmail.com>
2015-06-07 14:19:39 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,35 +1,33 @@
 @Echo OFF
-#C:\Users\Evan\Documents\Visual Studio 2013\Projects
-SET /P pathdir=Please enter your present working directory: 
-IF "%pathdir%"=="" GOTO Error
+set WORKING_DIRECTORY=%cd%
 
 Echo "Building solution/project file using batch file"
 SET PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319\
 SET SolutionPath=Foodie.Data.csproj
-cd %pathdir%\FoodieApp\Foodie.Data\
+cd %WORKING_DIRECTORY%\Foodie.Data\
 Echo Start Time - %Time%
 MSbuild %SolutionPath% /p:Configuration=Release  /t:Rebuild
 Echo End Time - %Time%
 
 SET SolutionPath=Foodie.Main.csproj
-cd %pathdir%\FoodieApp\FoodieApp\
+cd %WORKING_DIRECTORY%\FoodieApp\
 Echo Start Time - %Time%
 MSbuild %SolutionPath% /p:Configuration=Release  /t:Rebuild
 Echo End Time - %Time%
 
 SET SolutionPath=FoodieTest.csproj
-cd %pathdir%\FoodieApp\FoodieTest\
+cd %WORKING_DIRECTORY%\FoodieTest\
 Echo Start Time - %Time%
 MSbuild %SolutionPath% /t:Rebuild /p:Configuration=Release 
 Echo End Time - %Time%
 
-cd %pathdir%\FoodieApp\
+cd %WORKING_DIRECTORY%\FoodieApp\
 SET PATH=C:\Program Files\Microsoft Visual Studio 11.0\Common7\IDE\
 mstest /testcontainer:FoodieTest\bin\release\FoodieTest.dll"
 echo Build Process Completed...
 Set /p Wait=Press any key to start Foodie
 
-SET PATH=%pathdir%\FoodieApp\FoodieApp\bin\release\
+SET PATH=%WORKING_DIRECTORY%\FoodieApp\bin\release\
 Foodie.Main
 
 GOTO End
